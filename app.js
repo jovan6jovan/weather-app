98b0200eecedf861bae4d56cd16da2e66e1c2abe
jovan6jovan <jovan6jovanovic@gmail.com>
2020-04-14 21:09:36 +0200

    Prettier

--- app.js
+++ app.js
@@ -1,39 +1,40 @@
-window.addEventListener('load', () => {
-    let long;
-    let lat;
-    const degree = document.querySelector('.degree');
-    const tempDescription = document.querySelector('.temperature-description');
-    const timezone = document.querySelector('.location-timezone');
+window.addEventListener("load", () => {
+  let long;
+  let lat;
+  const degree = document.querySelector(".degree");
+  const tempDescription = document.querySelector(".temperature-description");
+  const timezone = document.querySelector(".location-timezone");
 
-    if(navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(position => {
-            long = position.coords.longitude;
-            lat = position.coords.latitude;
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
 
-            const proxy = 'https://cors-anywhere.herokuapp.com/';
+      const proxy = "https://cors-anywhere.herokuapp.com/";
 
-            const API = `${proxy}https://api.darksky.net/forecast/d4ba9f2e8326591e9ba6e832af6d9077/${lat},${long}`;
+      const API = `${proxy}https://api.darksky.net/forecast/d4ba9f2e8326591e9ba6e832af6d9077/${lat},${long}`;
 
-            fetch(API)
-            .then(response => response.json())
-            .then(data => {
-                // Destructuring data.currently object
-                const {temperature, summary, icon} = data.currently;
-                // Prikazujem konvertovanu temperaturu u celzijusima zato sto ovaj API daje samo farenhajte
-                let celsius = (temperature - 32) * (5 / 9);
-                degree.textContent = Math.floor(celsius);
-                tempDescription.textContent = summary;
-                timezone.textContent = data.timezone;
-                
-                setIcons(icon, document.querySelector('.icon'));
-            })
-        })
-    }
+      fetch(API)
+        .then((response) => response.json())
+        .then((data) => {
+          // Destructuring data.currently object
+          const { temperature, summary, icon } = data.currently;
+          // Prikazujem konvertovanu temperaturu u celzijusima zato sto ovaj API daje samo farenhajte
+          let celsius = (temperature - 32) * (5 / 9);
+          degree.textContent = Math.floor(celsius);
+          tempDescription.textContent = summary;
+          timezone.textContent = data.timezone;
 
-    function setIcons(icon, iconId) {
-        const skycons = new Skycons({color: "white"});
-        const currentIcon = icon.replace(/-/g, "_").toUpperCase(); // Zamenice svaki - sa _
-        skycons.play();
-        return skycons.set(iconId, Skycons[currentIcon]);
-    }
-})
+          setIcons(icon, document.querySelector(".icon"));
+        });
+    });
+  }
+
+  function setIcons(icon, iconId) {
+    const skycons = new Skycons({ color: "white" });
+    // Zamenice svaki - sa _
+    const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+    skycons.play();
+    return skycons.set(iconId, Skycons[currentIcon]);
+  }
+});
